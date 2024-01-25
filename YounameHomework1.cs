3929a8354e3e6155f1c191dffb1b30c6c89788f5
drcrooks1 <157441418+drcrooks1@users.noreply.github.com>
2024-01-25 13:25:40 -0600

    Homework1File

---
+++ YounameHomework1.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // linear eauqtion Z = 4X^2+3Y
+        double X = 2.5;
+        double Y = 3.3;
+        double Z = 4*X*X + 3 * Y; //34.9
+
+        //print x, y, z values
+        Console.WriteLine(X);
+        Console.WriteLine(Y);
+        Console.WriteLine(Z);
+
+        
+
+    }
+}
